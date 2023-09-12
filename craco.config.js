6b27bdcdb92810721530a1d565ea83877c3396a8
DALHOUM-Ahmed <dalhoum.co@gmail.com>
2023-09-12 16:57:36 +0100

    changed for test

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,28 @@
 // craco.config.js
 module.exports = {
   style: {
+    css: {
+      loaderOptions: {
+        sourceMap: false,
+      },
+    },
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+      loaderOptions: {
+        sourceMap: false,
+      },
     },
   },
-}
+  webpack: {
+    configure: {
+      resolve: {
+        alias: {
+          crypto: require.resolve("crypto-browserify"),
+        },
+        fallback: {
+          stream: require.resolve("stream-browserify"),
+        },
+      },
+    },
+  },
+};
